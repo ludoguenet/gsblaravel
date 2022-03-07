d0d474972e433a4a40896ffd3533a106869b7d28
ludoguenet <ludovicguenet@gmx.com>
2022-03-07 23:53:46 +0100

    uniqid

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,7 @@ class UserFactory extends Factory
         return [
             'last_name' => $this->faker->lastName(),
             'first_name' => $this->faker->firstName(),
-            'login' => uniqid(),
+            'login' => uniqid() + uniqid(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'address' => $this->faker->words(10, true),
             'zip_code' => $this->faker->postcode(),
