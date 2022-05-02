93fe455cd4438921eb00721ed95c7872e38a5ad2
ludoguenet <ludovicguenet@gmx.com>
2022-05-02 22:57:22 +0200

    Switch from laravel logo to GSB logo on login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,8 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
+                <img src="{{ asset('img/logo.jpg') }}" alt="Logo GSB" class="w-20">
             </a>
         </x-slot>
 
